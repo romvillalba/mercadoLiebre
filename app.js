bdd1939a1094f387f0347c1ed30f9ff7efb9d473
romvillalba <rominavillalbap5@gmail.com>
2023-05-21 12:20:55 -0300

    Subiendo hoja de estilos styles.css

---
+++ app.js
@@ -0,0 +1,23 @@
+//1°
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static('public')); 
+
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000");
+});
+
+//2°
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/home.html')) //linkeo con mi doc HTML
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
